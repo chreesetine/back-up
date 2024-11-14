3f99db545b9008b0fd7aeac5d276768192ec8cef
chreesetine <167083534+chreesetine@users.noreply.github.com>
2024-11-15 04:23:28 +0800

    Delete records directory

--- records/sidebar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.querySelector("#toggle-btn");
-
-    hamburger.addEventListener("click",function() {
-    document.querySelector("#sidebar").classList.toggle("expand");
-    });
-});
-
